ef5266b127f7079c29f559b4bf56ee2950da6abf
Andreas Jacobsen <anjac@itu.dk>
2017-06-10 10:43:45 +0200

    Removed settings

--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,7 @@ style('paper', 'style');
 <div id="app">
 	<div id="app-navigation">
 		<?php print_unescaped($this->inc('navigation/index')); ?>
-		<?php print_unescaped($this->inc('settings/index')); ?>
+		<?php //print_unescaped($this->inc('settings/index')); ?>
 	</div>
 
 	<div id="app-content">
