849c1de6f70a9de8af6d21c05f78cb7b9e9f8a32
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 20:11:04 +0200

    Forgot the if tags

--- templates/content/reader.php
+++ templates/content/reader.php
@@ -1,5 +1,8 @@
 <script id="content-template" type="text/x-jsrender">
+    {{if paper}}
+        <h1>{{:paper.title}}</h1>
         <p>{{:paper.content}}</p>
+    {{/if}}
 </script>
 
 <div id="paper-content"></div>
