9fabca327d9b503f44f07c24a641a538cea8708a
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 15:02:55 +0200

    test

--- templates/content/list.php
+++ templates/content/list.php
@@ -4,4 +4,6 @@ use andreskrey\Readability\HTMLParser;
 $readability = new HTMLParser();
 $html = file_get_contents('https://github.com/andreskrey/readability.php');
 $result = $readability->parse($html);
+
+echo $result->author;
 ?>
