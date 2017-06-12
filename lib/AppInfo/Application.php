4750df4d382f6865bc0bb98aff1ee62b9cc98758
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 14:04:13 +0200

    test

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -6,11 +6,9 @@
  * Time: 17.34
  */
 
-namespace OCA\Paper\AppInfo;
-
 //require_once __DIR__ . '/../../vendor/autoload.php';
 //require_once "../../lib/JSLikeHTMLElement.php";
 //require_once "../../lib/Readability.php";
 
 require_once '../../lib/JSLikeHTMLElement.php';
-require_once '../../lib/Readability.php';
+//require_once '../../lib/Readability.php';
