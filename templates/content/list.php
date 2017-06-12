86c8cb0387e661f9454e59de620bacec68db46da
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 15:00:56 +0200

    Testing

--- templates/content/list.php
+++ templates/content/list.php
@@ -1,1 +1,8 @@
 <h1>Hello world, this is the list of articles.</h1>
+
+
+<?php
+$readability = new HTMLParser();
+$html = file_get_contents('https://github.com/andreskrey/readability.php');
+$result = $readability->parse($html);
+?>
