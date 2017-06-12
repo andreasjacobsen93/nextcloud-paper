7d04e9f2b8f81bd5554cd5108a869d861b07233f
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 18:24:00 +0200

    testing

--- js/script.js
+++ js/script.js
@@ -1,81 +1,76 @@
 
-(function (OC, window, $, undefined) {
-    'use strict';
+$(document).ready(function () {
 
-    $(document).ready(function () {
+    var source = $('#navigation-template').html();
+    var template = Handlebars.compile(source);
 
-        var source = $('#navigation-template').html();
-        var template = Handlebars.compile(source);
+    // this notes object holds all our notes
+    /*var Papers = function (baseUrl) {
+        this._baseUrl = baseUrl;
+        this._papers = [];
+        this._activePaper = undefined;
+    };
 
-        // this notes object holds all our notes
-        /*var Papers = function (baseUrl) {
-            this._baseUrl = baseUrl;
-            this._papers = [];
-            this._activePaper = undefined;
-        };
+    Papers.prototype = {
+        load: function (id) {
+            var self = this;
+            this._papers.forEach(function (paper) {
+                if (paper.id === id) {
+                    paper.active = true;
+                    self._activeNote = paper;
+                } else {
+                    paper.active = false;
+                }
+            });
+        },
+        getActive: function () {
+            return this._activePaper;
+        },
+        getAll: function () {
+            return this._papers;
+        },
+        loadAll: function () {
+            var deferred = $.Deferred();
+            var self = this;
+            $.get(this._baseUrl).done(function (papers) {
+                self._activePaper = undefined;
+                self._papers = papers;
+                deferred.resolve();
+            }).fail(function () {
+                deferred.reject();
+            });
+            return deferred.promise();
+        }
+    };
 
-        Papers.prototype = {
-            load: function (id) {
-                var self = this;
-                this._papers.forEach(function (paper) {
-                    if (paper.id === id) {
-                        paper.active = true;
-                        self._activeNote = paper;
-                    } else {
-                        paper.active = false;
-                    }
-                });
-            },
-            getActive: function () {
-                return this._activePaper;
-            },
-            getAll: function () {
-                return this._papers;
-            },
-            loadAll: function () {
-                var deferred = $.Deferred();
-                var self = this;
-                $.get(this._baseUrl).done(function (papers) {
-                    self._activePaper = undefined;
-                    self._papers = papers;
-                    deferred.resolve();
-                }).fail(function () {
-                    deferred.reject();
-                });
-                return deferred.promise();
-            }
-        };
+    // this will be the view that is used to update the html
+    var View = function (papers) {
+        this._papers = papers;
+    };
 
-        // this will be the view that is used to update the html
-        var View = function (papers) {
-            this._papers = papers;
-        };
+    View.prototype = {
+        renderContent: function () {
+        },
+        renderList: function () {
+            var source = $('#list-tpl').html();
+            var template = Handlebars.compile(source);
+        },
+        renderNavigation: function () {
+            var source = $('#navigation-tpl').html();
+            var template = Handlebars.compile(source);
+        },
+        render: function () {
+            //this.renderContent();
+            //this.renderList();
+            this.renderNavigation();
+        }
+    };
 
-        View.prototype = {
-            renderContent: function () {
-            },
-            renderList: function () {
-                var source = $('#list-tpl').html();
-                var template = Handlebars.compile(source);
-            },
-            renderNavigation: function () {
-                var source = $('#navigation-tpl').html();
-                var template = Handlebars.compile(source);
-            },
-            render: function () {
-                //this.renderContent();
-                //this.renderList();
-                this.renderNavigation();
-            }
-        };
-
-        var papers = new Papers(OC.generateUrl('/apps/paper/papers'));
-        var view = new View(papers);
-        //papers.loadAll().done(function () {
-            view.render();
-        //}).fail(function () {
-        //    alert('Could not load papers');
-        //});*/
-    });
-
-})(OC, window, jQuery);
+    var papers = new Papers(OC.generateUrl('/apps/paper/papers'));
+    var view = new View(papers);
+    //papers.loadAll().done(function () {
+        view.render();
+    //}).fail(function () {
+    //    alert('Could not load papers');
+    //});*/
+});
