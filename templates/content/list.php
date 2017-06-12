7571bd0157542eebc2b4330245d59ca28dffcec5
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 19:29:08 +0200

    Added content

--- templates/content/list.php
+++ templates/content/list.php
@@ -1,9 +1,9 @@
 <script id="list-template" type="text/x-jsrender">
     {{for  papers}}
     <li class="paper-item {{if active}}active{{/if}}" data-id="{{ id }}">
-        <h1>{{:title}}</h1>
-        <p>{{:site}}</p>
-        <p>{{:description}}</p>
+        <h1 class="paper-item-title">{{:title}}</h1>
+        <p class="paper-item-info">{{:site}} - {{:author}} - {{:published}}</p>
+        <p class="paper-item-description">{{:description}}</p>
     </li>
     {{/for}}
 </script>
