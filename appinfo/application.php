21416c571e03793ad4abb4a460e8a852d63d5f4d
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 17:28:39 +0200

    testing

---
+++ appinfo/application.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: fsociety
+ * Date: 12/06/2017
+ * Time: 17.26
+ */
+class Application extends App {
+
+    public function __construct(array $urlParams=array()){
+        parent::__construct('myapp', $urlParams);
+
+        $container = $this->getContainer();
+
+        /**
+         * Controllers
+         */
+        $container->registerService('PageController', function($c) {
+            return new PageController(
+                $c->query('AppName'),
+                $c->query('Request')
+            );
+        });
+        $container->registerService('PaperController', function($c) {
+            return new PaperController(
+                $c->query('AppName'),
+                $c->query('Request')
+            );
+        });
+        $container->registerService('PaperApiController', function($c) {
+            return new PaperApiController(
+                $c->query('AppName'),
+                $c->query('Request')
+            );
+        });
+    }
+
+}
