30bba884636b598915f1a5c48a4683cf89a10169
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 15:13:12 +0200

    Testing data extract

--- templates/content/list.php
+++ templates/content/list.php
@@ -7,4 +7,21 @@ $info = Embed::create('https://www.gnu.org/software/gnuzilla/');
 
 //Get content info
 
-echo $info->title; //The page title
+echo $info->title.'<br>'; //The page title
+echo $info->description.'<br>'; //The page description
+echo $info->url.'<br>'; //The canonical url
+echo $info->type.'<br>'; //The page type (link, video, image, rich)
+echo $info->tags.'<br>'; //The page keywords (tags)
+
+echo $info->authorName.'<br>'; //The resource author
+echo $info->authorUrl.'<br>'; //The author url
+
+echo $info->providerName.'<br>'; //The provider name of the page (Youtube, Twitter, Instagram, etc)
+echo $info->providerUrl.'<br>'; //The provider url
+echo $info->providerIcons.'<br>'; //All provider icons found in the page
+echo $info->providerIcon.'<br>'; //The icon choosen as main icon
+
+echo $info->publishedDate.'<br>'; //The published date of the resource
+echo $info->license.'<br>'; //The license url of the resource
+echo $info->linkedData.'<br>'; //The linked-data info (http://json-ld.org/)
+echo $info->feeds; //The RSS/Atom feeds
