f564a9dc99f23bce792b606a6dd9e6376e6b8038
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 14:02:10 +0200

    Testing deprecated autloader

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -11,5 +11,9 @@ namespace OCA\Paper\AppInfo;
 //require_once __DIR__ . '/../../vendor/autoload.php';
 //require_once "../../lib/JSLikeHTMLElement.php";
 //require_once "../../lib/Readability.php";
+
+require_once '/apps/paper/lib/JSLikeHTMLElement.php';
+require_once '/apps/paper/lib/Readability.php';
+
 use \OCA\Paper\JSLikeHTMLElement;
 use \OCA\Paper\Readability;
