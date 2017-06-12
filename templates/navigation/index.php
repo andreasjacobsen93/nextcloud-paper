9034cd0554a06675a07694eea01ba850342846ac
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 19:05:03 +0200

    Add button

--- templates/navigation/index.php
+++ templates/navigation/index.php
@@ -1,8 +1,10 @@
+
+<form id="add_form">
+    <input type="text" id="add_url"/>
+    <button id="bookmark_add_submit" title="Add" class="icon-add"></button>
+</form>
+
 <ul id="navigation-list">
     <li>
-        <form id="add_form">
-            <input type="text" id="add_url" value="" placeholder="<?php p($l->t('Address')); ?>"/>
-            <button id="bookmark_add_submit" title="Add" class="icon-add"></button>
-        </form>
     </li>
 </ul>
