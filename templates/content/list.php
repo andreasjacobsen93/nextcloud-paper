42c87b0ed2963c2df2ac8c60650b21dcdf47eed5
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 16:50:05 +0200

    Updated handlebars.js

--- templates/content/list.php
+++ templates/content/list.php
@@ -1,4 +1,4 @@
-<script id="navigation-tpl" type="text/x-handlebars-template">
+<script id="list-tpl" type="text/x-handlebars-template">
     {{#each papers}}
     <li class="note with-menu {{#if active}}active{{/if}}" data-id="{{ id }}">
         <a href="#">{{ title }}</a>
