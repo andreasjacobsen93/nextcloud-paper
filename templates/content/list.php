304f8c0d29c36c1e5ecd16740e63d6cc05115ba7
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 18:56:20 +0200

    Fix

--- templates/content/list.php
+++ templates/content/list.php
@@ -1,6 +1,6 @@
 <script id="list-template" type="text/x-jsrender">
     {{for  papers}}
-    <li class="note with-menu {{#if active}}active{{/if}}" data-id="{{ id }}">
+    <li class="note with-menu {{if active}}active{{/if}}" data-id="{{ id }}">
         <a href="#">{{:title}}</a>
     </li>
     {{/for}}
