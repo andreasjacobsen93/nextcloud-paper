09b40c23798a95c3859ed1cee30876804d99e41e
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 14:06:41 +0200

    Trying again...

--- templates/content/reader.php
+++ templates/content/reader.php
@@ -1,10 +1,10 @@
 <?php
 
-/*$doc = new Readability();
+$doc = new Readability();
 $doc->input('https://www.gnu.org/software/gnuzilla/');
 $doc->init();
 
 $content = $doc->getContent();
 
 echo '<h1>'.$doc->articleTitle->innerHTML.'</h1>';
-echo $doc->articleContent->innerHTML;*/
+echo $doc->articleContent->innerHTML;
