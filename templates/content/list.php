26657ac13786f94586b5eb34472fe11752cfa433
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 15:05:35 +0200

    test

--- templates/content/list.php
+++ templates/content/list.php
@@ -2,8 +2,8 @@
 use andreskrey\Readability\HTMLParser;
 
 $readability = new HTMLParser();
-$html = file_get_contents('https://github.com/andreskrey/readability.php');
+$html = file_get_contents('https://www.gnu.org/software/gnuzilla/');
 $result = $readability->parse($html);
 
-echo $result->article;
+echo $result['article'];
 ?>
