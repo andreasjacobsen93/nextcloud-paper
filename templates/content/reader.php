58f59e44ecef75359a294638800a8ab84455e169
Andreas Jacobsen <anjac@itu.dk>
2017-06-12 13:56:35 +0200

    More testing

--- templates/content/reader.php
+++ templates/content/reader.php
@@ -1,2 +1,11 @@
 <?php
 use \OCA\Paper\Readability;
+
+$doc = new Readability();
+$doc->input('https://www.gnu.org/software/gnuzilla/');
+$doc->init();
+
+$content = $doc->getContent();
+
+echo '<h1>'.$doc->articleTitle->innerHTML.'</h1>';
+echo $doc->articleContent->innerHTML;
