4acc6fd457bc048a9451d5b64ddfa7e5ded62fd8
Andreas Jacobsen <anjac@itu.dk>
2017-06-13 16:54:07 +0200

    Added Readability as lib instead

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -7,5 +7,5 @@
  */
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../JSLikeHTMLElement.php';
-require_once __DIR__ . '/../Readability.php';
+require_once __DIR__ . '/../Readability/JSLikeHTMLElement.php';
+require_once __DIR__ . '/../Readability/Readability.php';
